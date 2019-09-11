cedb2f9499949536c6305c02e524dc2a5c0c5363
lilei <lilei@sojoline.com>
2019-09-11 10:57:00 +0800

    嵌入式端作为主机，读写线圈状态，寄存器值程序测试通过

--- src/Modbus/unit-test.h
+++ src/Modbus/unit-test.h
@@ -25,24 +25,17 @@
 #define SERVER_ID         1
 #define INVALID_SERVER_ID 18
 
-// const uint16_t UT_BITS_ADDRESS = 0x130;
-// const uint16_t UT_BITS_NB = 0x25;
-// const uint8_t UT_BITS_TAB[] = { 0xCD, 0x6B, 0xB2, 0x0E, 0x1B };
+
 const uint16_t UT_BITS_ADDRESS = 0x0;
 const uint16_t UT_BITS_NB = 0x10;
 const uint8_t UT_BITS_TAB[] = {0xA5};
 
-// const uint16_t UT_INPUT_BITS_ADDRESS = 0x1C4;
-// const uint16_t UT_INPUT_BITS_NB = 0x16;
-// const uint8_t UT_INPUT_BITS_TAB[] = { 0xAC, 0xDB, 0x35 };
+
 const uint16_t UT_INPUT_BITS_ADDRESS = 0x10;
 const uint16_t UT_INPUT_BITS_NB = 0x10;
 const uint8_t UT_INPUT_BITS_TAB[] = {0xDB};
 
-// const uint16_t UT_REGISTERS_ADDRESS = 0x160;
-// const uint16_t UT_REGISTERS_NB = 0x3;
-// const uint16_t UT_REGISTERS_NB_MAX = 0x20;
-// const uint16_t UT_REGISTERS_TAB[] = { 0x022B, 0x0001, 0x0064 };
+
 const uint16_t UT_REGISTERS_ADDRESS = 0x20;
 const uint16_t UT_REGISTERS_NB = 0xA;
 const uint16_t UT_REGISTERS_NB_MAX = 0x10;
@@ -62,9 +55,7 @@ const uint16_t UT_REGISTERS_ADDRESS_BYTE_SLEEP_5_MS = 0x173;
    UT_REGISTERS_NB_POINTS to try to raise a segfault. */
 const uint16_t UT_REGISTERS_NB_SPECIAL = 0x2;
 
-// const uint16_t UT_INPUT_REGISTERS_ADDRESS = 0x108;
-// const uint16_t UT_INPUT_REGISTERS_NB = 0x1;
-// const uint16_t UT_INPUT_REGISTERS_TAB[] = { 0x000A };
+
 const uint16_t UT_INPUT_REGISTERS_ADDRESS = 0x30;
 const uint16_t UT_INPUT_REGISTERS_NB = 0x8;
 const uint16_t UT_INPUT_REGISTERS_TAB[] = {0x0001, 0x0020, 0x0300, 0x4000, 0x0055, 0x0660, 0x7700, 0x0808};
